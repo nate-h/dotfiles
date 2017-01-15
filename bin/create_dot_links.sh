1948d29ecc2a679325c5f012e30727266ca56940
nate <nhapeman@gmail.com>
2017-01-14 16:40:01 -0800

    get stupid windows symbolic links to work.

--- bin/create_dot_links.sh
+++ bin/create_dot_links.sh
@@ -10,6 +10,38 @@
 ################################################################################
 
 
+# We still need this.
+windows() { [[ -n "$WINDIR" ]]; }
+
+# Cross-platform symlink function. With one parameter, it will check
+# whether the parameter is a symlink. With two parameters, it will create
+# a symlink to a file or directory, with syntax: link $linkname $target
+link() {
+    if [[ -z "$2" ]]; then
+        # Link-checking mode.
+        if windows; then
+            fsutil reparsepoint query "$1" > /dev/null
+        else
+            [[ -h "$1" ]]
+        fi
+    else
+        # Link-creation mode.
+        if windows; then
+            # Windows needs to be told if it's a directory or not. Infer that.
+            # Also: note that we convert `/` to `\`. In this case it's necessary.
+            if [[ -d "$2" ]]; then
+                cmd <<< "mklink /D \"$1\" \"${2//\//\\}\"" > /dev/null
+            else
+                # cmd <<< "echo \"${1//\//\\}\" \"${2//\//\\}\""
+                cmd <<< "mklink ${1//\//\\} ${2//\//\\}"  > /dev/null
+            fi
+        else
+            ln -s "$2" "$1"
+        fi
+        echo "Created symbolic"
+    fi
+}
+
 
 # see if the parent directory is located in home directory
 echo -e "Attempting to create symbolic links for dot files\n"
@@ -22,7 +54,7 @@ for i in "${dotfiles[@]}"; do
   if [ -f ~/$i ]; then
       echo "Error: Unable to create symbolic link for $i. File already exist."
     else
-      ln -s ~/dotfiles/$i ~/$i
-      echo "Created symbolic"
+      # ln -s ~/dotfiles/$i ~/$i
+      link %HOMEPATH%/$i %HOMEPATH%/dotfiles/$i
   fi
 done
