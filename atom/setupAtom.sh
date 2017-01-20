f0cacd626aa70b1e44c1228f06d598b1a3143c93
nate <nhapeman@gmail.com>
2017-01-20 06:32:47 -0800

    make atm setup bare min

--- atom/setupAtom.sh
+++ atom/setupAtom.sh
@@ -4,11 +4,6 @@
 ###############################################################################
 
 
-# Atom editor settings
-echo -n "Copying Atom settings.."
-mv -f ~/.atom ~/dotfiles_old/
-ln -s $HOME/dotfiles/atom ~/.atom
-echo "done"
 
 # Install personal packages
 apm install --packages-file $HOME/.atom/packages.list
