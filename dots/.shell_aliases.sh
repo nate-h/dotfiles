cde4e0beea3bcc40f74e1f3f3aba592804b563ca
nate_h <nhapeman@gmail.com>
2017-02-20 13:51:47 -0800

    remove redundant calls. Load new dot files. setup zsh to use them.

--- dots/.shell_aliases.sh
+++ dots/.shell_aliases.sh
@@ -3,8 +3,14 @@
 # users are encouraged to define aliases within the ZSH_CUSTOM folder.
 # For a full list of active aliases, run `alias`.
 
+# open with no output
+function open(){
+	gnome-open "$@" >/dev/null 2>&1
+}
+
 # Enable aliases to be sudo’ed
 alias sudo="sudo "
+
 
 # Shortcuts
 alias o="open"
@@ -16,55 +22,82 @@ alias gh="github"
 alias x+="chmod +x"
 alias -- +x="chmod +x"
 
-# Easier navigation: .., ..., ~ and -
-alias ..="cd .."
+alias less='less -r'                          # raw control characters
+alias whence='type -a'                        # where, of a sort
+alias grep='grep --color'                     # show differences in colour
+alias egrep='egrep --color=auto'              # show differences in colour
+alias fgrep='fgrep --color=auto'              # show differences in colour
+
+alias path='echo -e ${PATH//:/\\n}'
+alias dir='ls --color=auto --format=vertical'
+alias vdir='ls --color=auto --format=long'
+
+################################################################################
+#######################   Basic Navigation Commands   ##########################
+################################################################################
+
 alias cd..="cd .."
+alias ..="cd .."
 alias ...="cd ../.."
 alias ....="cd ../../.."
 alias .....="cd ../../../.."
-alias ~="cd ~" # `cd` is probably faster to type though
+alias ......="cd ../../../../.."
+alias desk="cd ~/Desktop"
+alias dev="cd ~/dev"
 alias -- -="cd -"
 
-# mv, rm, cp, gunzip
-alias mv='mv -v'
-alias rm='rm -i -v'
-alias cp='cp -v'
+################################################################################
+##################################   Misc   ####################################
+################################################################################
+
+
+# gunzip
 alias ungz="gunzip -k"
 
 # Trim new lines and copy to clipboard
 alias trimcopy="tr -d '\n' | pbcopy"
-
-alias cask='brew cask' # i <3 u cask
 alias where=which # sometimes i forget
-alias brwe=brew  #typos
-
-# Vagrant aliases
-alias vag='vagrant'
-alias vagup='vagrant up'
-alias vagdestroy='vagrant destroy'
-alias vagssh='vagrant ssh'
-alias vaghalt='vagrant halt'
 
 # Be nice
 alias htop='sudo htop'
 alias hosts='sudo $EDITOR /etc/hosts'   # yes I occasionally 127.0.0.1 twitter.com ;)
 
-#
-# Time to upgrade `ls`
-#
 
-# Use coreutils `ls` if possible
-hash gls >/dev/null 2>&1 || alias gls="ls"
+# View HTTP traffic
+alias sniff="sudo ngrep -W byline -d 'en0' -t '^(GET|POST) ' 'tcp and port 80'"
+alias httpdump="sudo tcpdump -i en0 -n -s 0 -w - | grep -a -o -E \"Host\: .*|GET \/.*\""
 
-# Always use color output for `ls`
-# Detect which `ls` flavor is in use
-if gls --color > /dev/null 2>&1; then # GNU `ls`
-  colorflag="--color"
-else # OS X `ls`
-  colorflag="-G"
-fi
+# Enhanced WHOIS lookups
+alias whois="whois -h whois-servers.net"
 
-export CLICOLOR_FORCE=1
+# Download file and save it with filename of remote file
+alias get="curl -O -L"
+
+
+# File size
+alias fs="stat -f \"%z bytes\""
+
+
+# zshrc config
+alias zshrc="${EDITOR} ${$(cd -P $(dirname $0) && cd ..; pwd -P)}"
+alias reload="source ~/.zshrc && echo 'Shell config reloaded from ~/.zshrc'"
+
+# zsh.local config
+alias localrc="if [[ -a ~/.zsh.local ]]; then ${EDITOR} ~/.zsh.local; fi"
+
+
+# HTTP Requests. One of @janmoesen’s ProTip™s
+for method in GET HEAD POST PUT DELETE TRACE OPTIONS; do
+  alias "$method"="lwp-request -m '$method'"
+done
+
+################################################################################
+################################   Ls Styling   ################################
+################################################################################
+
+alias ll="ls -lv --group-directories-first"
+alias la='ls -a | grep "^\."'
+
 
 # List all files colorized in long format
 alias l="ls -lF ${colorflag}"
@@ -90,9 +123,6 @@ alias myip="ifconfig | grep 'inet ' | grep -v 127.0.0.1 | awk '{print \$2}'"
 # Copy my public key to the pasteboard
 alias pubkey="more ~/.ssh/id_rsa.pub | pbcopy | printf '=> Public key copied to pasteboard.\n'"
 
-# Flush DNS cache
-alias flushdns="dscacheutil -flushcache"
-
 #
 # GIT STUFF
 #
@@ -108,13 +138,28 @@ function git_list_branches() {
   done | sort;
 }
 
-# Git aliases
-# More Git tips http://firstaidgit.io/
+################################################################################
+############################   Git Shortscuts   ################################
+################################################################################
+
+alias gupdate="git submodule update --init --recursive"
+alias fetch="git fetch"
+alias pull="git pull"
+alias push="git push"
+alias rebase="git rebase origin master"
+alias stash="git stash"
+alias status="git status"
+alias gk="gitk --all"
+alias gui="git gui"
+alias gs="git status"
+alias gco="git checkout"
+alias gb="git branch"
+
+
 alias gss="git status -s"
 alias gp="git push origin HEAD"
 alias gpt="git push origin HEAD && git push --tags"
 alias wip="git commit -m'WIP' . && git push origin HEAD"
-alias grok="ngrok start rem.jsbin-dev.com static.rem.jsbin-dev.com learn.rem.jsbin-dev.com"
 alias gl='git log'
 alias glp5='git log -5 --pretty --oneline' # view your last 5 latest commits each on their own line
 alias glt='git log --all --graph --decorate --oneline --simplify-by-decoration' # pretty branch status
@@ -147,59 +192,3 @@ alias undopush="git push -f origin HEAD^:master"
 
 # `cd` to Git repo root
 alias gr='[ ! -z `git rev-parse --show-cdup` ] && cd `git rev-parse --show-cdup || pwd`'
-
-# Gist
-alias gist-paste="gist --private --copy --paste --filename"  # gist-paste filename.ext -- create private Gist from the clipboard contents
-alias gist-file="gist --private --copy"  # gist-file filename.ext -- create private Gist from a file
-
-# Networking. IP address, dig, DNS
-alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
-alias dig="dig +nocmd any +multiline +noall +answer"
-
-# View HTTP traffic
-alias sniff="sudo ngrep -W byline -d 'en0' -t '^(GET|POST) ' 'tcp and port 80'"
-alias httpdump="sudo tcpdump -i en0 -n -s 0 -w - | grep -a -o -E \"Host\: .*|GET \/.*\""
-
-# Enhanced WHOIS lookups
-alias whois="whois -h whois-servers.net"
-
-# Download file and save it with filename of remote file
-alias get="curl -O -L"
-
-# Recursively delete `.DS_Store` files
-alias cleanup_dsstore="find . -name '*.DS_Store' -type f -ls -delete"
-
-# Disc utils
-alias diskspace_report="df -P -kHl"
-alias free_diskspace_report="diskspace_report"
-
-# File size
-alias fs="stat -f \"%z bytes\""
-
-# Empty the Trash on all mounted volumes and the main HDD.
-# Also, clear Apple’s System Logs & the useless sleepimage to improve shell startup speed
-alias emptytrash="sudo rm -rfv /Volumes/*/.Trashes; sudo grm -rf $HOME/.Trash; sudo rm -rtv /private/var/log/asl/*.asl; sudo rm /private/var/vm/sleepimage"
-
-# Show/hide hidden files in Finder
-alias showdotfiles="defaults write com.apple.finder AppleShowAllFiles -bool true && killall Finder"
-alias hidedotfiles="defaults write com.apple.finder AppleShowAllFiles -bool false && killall Finder"
-
-# Show/hide all desktop icons (useful when presenting)
-alias showdesktop="defaults write com.apple.finder CreateDesktop -bool true && killall Finder"
-alias hidedesktop="defaults write com.apple.finder CreateDesktop -bool false && killall Finder"
-
-# zshrc config
-alias zshrc="${EDITOR} ${$(cd -P $(dirname $0) && cd ..; pwd -P)}"
-alias reload="source ~/.zshrc && echo 'Shell config reloaded from ~/.zshrc'"
-
-# zsh.local config
-alias localrc="if [[ -a ~/.zsh.local ]]; then ${EDITOR} ~/.zsh.local; fi"
-
-# Update installed Ruby gems, Homebrew, npm, and their installed packages
-alias brew_update="brew -v update; brew upgrade --force-bottle --cleanup; brew cleanup; brew cask cleanup; brew prune; brew doctor; npm-check -g -u"
-alias update_brew_npm_gem='brew_update; npm install npm -g; npm update -g; sudo gem update --system; sudo gem update --no-rdoc --no-ri'
-
-# HTTP Requests. One of @janmoesen’s ProTip™s
-for method in GET HEAD POST PUT DELETE TRACE OPTIONS; do
-  alias "$method"="lwp-request -m '$method'"
-done
