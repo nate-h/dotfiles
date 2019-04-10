713a70ab2f875bc2cc6a1bc5df79c56d67a5a87a
nates linux desktop <nhapeman@appliedinvention.com>
2019-04-10 14:23:15 -0700

    Update install script

--- installEverything.sh
+++ installEverything.sh
@@ -22,50 +22,50 @@ done
 # create dot links
 source ~/dotfiles/bin/create_dot_links.sh
 
-# reset atom
-#rm -rf ~/.atom
-#rm -rf ~/dotfiles/atom/packages
-#source ~/dotfiles/atom/create_atom_link.sh
-#source ~/dotfiles/atom/setupAtom.sh
+# download chrome
+
+# generate ssh key
+
+# install git
+# git clone git@github.com:nate-h/mypad.git
+# git clone git@github.com:nate-h/dotfiles.git
+
+# create dev folder
+
+# sudo apt install curl
+# sudo apt-get install git-gui
+
+# sudo apt-get update
+# sudo apt-get install vim
 
 
-#download chrome
+# sudo apt-get update
+# sudo apt-get install terminator
 
-#generate ssh key
+##############################################################################
+# Blue tooth 
+##############################################################################
 
-#install git
+# https://medium.com/@overcode/fixing-bluetooth-in-ubuntu-pop-os-18-04-d4b8dbf7ddd6
 
-#create dev folder
+##############################################################################
+# Zsh 
+##############################################################################
 
-#install atom
-#install curl
-
-#sudo apt-get install git-gui
-
-#sudo apt-get update
-#sudo apt-get install vim
-
-
-#sudo apt-get update
-#sudo apt-get install terminator
-
-#######################
-#install zsh
-#sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+# sudo apt install zsh
+# sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 # chsh -s $(which zsh)
 
 # Install syntax highlighting.
-#git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
- #echo "source ${(q-)PWD}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
+# https://github.com/zsh-users/zsh-syntax-highlighting/blob/master/INSTALL.md
+# git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+# echo "source ${(q-)PWD}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
 
 
-
-
-#sudo apt-get install python-setuptools
-
-#sudo easy_install pip
-
-#sudo pip install virtualenvwrapper
+# Install 
+# sudo apt-get install python-setuptools
+# sudo apt-get install python-pip
+# sudo pip install virtualenvwrapper
 
 # Folow this:
 # https://gist.github.com/renshuki/3cf3de6e7f00fa7e744a
@@ -77,3 +77,11 @@ source ~/dotfiles/bin/create_dot_links.sh
 # Do this next:
 #I recommend you option 1 (dark theme) which is just great.
 #Choose option 1 to download seebi' dircolors-solarized
+
+#####################################
+# vs code
+#####################################
+
+# Install vs code through app store
+# Run in working vs code and copy over list:
+# code --list-extensions | xargs -L 1 echo code --install-extension
