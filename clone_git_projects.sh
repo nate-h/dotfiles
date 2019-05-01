996eb55108e3422c283ece8a5e6ad7681d11c3f6
nates linux desktop <nhapeman@appliedinvention.com>
2019-04-30 20:01:52 -0700

    add git projects install script

---
+++ clone_git_projects.sh
@@ -0,0 +1,12 @@
+cd ~
+mkdir -p dev
+cd dev
+git clone git@github.com:nate-h/nhapeman.com_v3.git
+git clone git@github.com:nate-h/resume.git
+git clone git@github.com:nate-h/minesweeper_js.git
+git clone git@github.com:nate-h/pacman_js.git
+git clone git@github.com:nate-h/MyTunes.git
+git clone git@github.com:nate-h/mario.git
+git clone git@github.com:nate-h/riemann.git
+git clone git@github.com:nate-h/pixel-chaser.git
+git clone git@github.com:nate-h/dogsite.git
